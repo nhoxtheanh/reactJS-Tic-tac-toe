f9fd34748ba960e2d11cae3c1b861a2640ccc007
The Anh <nhoxtheanh@gmail.com>
2020-10-20 23:03:14 +0700

    Square function component & react-folder-structure

--- src/index.js
+++ src/index.js
@@ -1,24 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-
-// class Square extends React.Component {
-//   render() {
-//     return (
-//         <button className="square" onClick={() => this.props.onClick()}>
-//           {this.props.value}
-//         </button>
-//       );
-//     }
-// }
-function Square(props){
-  const className = 'square' + (props.highlight ? ' highlight' : '');
-  return (
-    <button className={className} onClick = {props.onClick}>
-        {props.value}
-    </button>
-  );
-}
+import Square from './components/Square';
   
 class Board extends React.Component {
   renderSquare(i) {
